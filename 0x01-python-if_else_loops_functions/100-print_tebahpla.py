c025f1ebfa9ab4e9cfbabff1a057a2bd7c5ddcff
Stacy-Muhia <stacymuhiap@gmail.com>
2023-08-08 19:05:16 -0700

    corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(122, 96, -1):
-    print(chr(char).upper() if char % 2 == 0 else chr(char).lower(), end="")
+    print("{:c}".format(char).upper()
+            if char % 2 == 0 else "{:c}".format(char).lower(), end="")
