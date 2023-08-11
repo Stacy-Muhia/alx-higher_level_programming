56d5676ab8f7ebd043dca5ba9046e09428462864
Stacy-Muhia <stacymuhiap@gmail.com>
2023-08-11 13:32:08 -0700

    corrected

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python33
-
-import sys
+#!/usr/bin/python3
 
 if __name__ == "__main__":
+    import sys
+
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
