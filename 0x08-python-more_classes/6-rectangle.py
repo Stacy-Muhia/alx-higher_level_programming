4ad4790c5d3e827e450aeae34d7aaf11fbf6bb9f
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-04 08:29:58 -0700

    corrected

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,12 +6,14 @@
 
 class Rectangle:
     """class rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """private instance arrtribute of width and height"""
     
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
