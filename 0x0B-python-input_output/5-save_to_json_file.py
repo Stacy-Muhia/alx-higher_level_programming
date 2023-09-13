abaefb3fcf812e280ee66c460523f4768b6fba15
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-13 09:23:27 -0700

    catching up mazee

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,11 +6,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-        writes an Object to a text file, using a JSON representation
-        
-        must use the with statement
-
-        don’t need to manage exceptions and file permission exceptions
+        writes an Object to a text file, using a JSON rep
     """
     with open(filename, "w") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,13 +6,7 @@ import json
 
 def load_from_json_file(filename):
     """
-       function that creates an Object from a JSON file
-
-       must use the with statement
-
-       don’t need to manage exceptions if the JSON string doesn’t represent an object.
-
-       don’t need to manage file permissions / exceptions.
+       function that creates an Object
     """
     with open(filename) as f:
         return json.load(f)
