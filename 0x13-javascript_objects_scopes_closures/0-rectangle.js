c4486747a836eac760f5f851a98829a998297f6a
Stacy-Muhia <stacymuhiap@gmail.com>
2023-11-14 11:39:36 -0800

    all tasks

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+class Rectangle {
+//this is an empty class
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+!/usr/bin/node
+//contains height and weight === 0
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
