f7f6e7211f5b996c7abac08bccf76b82516943ee
Stacy-Muhia <stacymuhiap@gmail.com>
2024-01-03 13:30:10 -0800

    task one

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
-!/usr/bin/node
-
+#!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
 
-fs.writeFile(file, data, (err) => {
-  if (err) throw err;
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
